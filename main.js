8e980898a7998259ca899a40d5b7cf1541f75530
GiancarloLadisa <124166799+GiancarloLadisa@users.noreply.github.com>
2023-02-21 18:21:03 +0100

    Add files via upload
    inizio JS

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log('Hello word!')
+showcontact = document.querySelector('#btn')
+
+const rubrica = {
+
+    contatti : [
+
+        { name : 'Giancarlo' , number : 33333333333},
+
+        { name : 'Leonardo' , number : 4444444444444},
+
+        { name : 'Giammario' , number : 111111111111},
+
+        { name : 'Fabio' , number : 222222222222222},
+
+
+        
+    ]
+} 
